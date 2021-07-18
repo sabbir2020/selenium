37078d5640f6e63c3a28fa6f64924742edf718a8
Mohammad Ma Ahmed <mohammad.ma.ahmed@gmail.com>
2021-07-18 17:26:40 -0400

    new files

---
+++ src/main/java/com/ahmed/tutorial/App.java
@@ -0,0 +1,33 @@
+package com.ahmed.tutorial;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        // find the driver and use that
+        WebDriverManager.chromedriver().setup();
+       
+
+        // step 2: create driver instance
+        WebDriver driver = new ChromeDriver();
+
+        // step 3: invoke get method
+        driver.get("https://www.amazon.com");
+
+        // close browser
+        driver.close();
+    	
+    	
+    	
+    }
+}

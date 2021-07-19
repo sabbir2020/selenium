789368c023cfe0160205ceccd2debac10816d618
Mohammad Ma Ahmed <mohammad.ma.ahmed@gmail.com>
2021-07-19 18:36:19 -0400

    added a new test

---
+++ src/main/java/com/ahmed/tutorial/Yahoo.java
@@ -0,0 +1,26 @@
+package com.ahmed.tutorial;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Yahoo {
+
+    public static void main( String[] args ) {
+
+        WebDriverManager.chromedriver(). setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.yahoo.com/");
+
+
+
+
+
+    }
+
+
+
+
+}
